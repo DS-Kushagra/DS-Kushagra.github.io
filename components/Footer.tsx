4261a3612a0578251ef8fbc4b69c3ea33b7c0a76
Kushagra <kushagraagrawal128@gmail.com>
2024-08-26 00:25:22 +0530

    Initial Commit

---
+++ components/Footer.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { MagicButton } from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data';
+import { profile } from 'console';
+import { link } from 'fs';
+
+const Footer = () => {
+  return (
+    <footer className="w-full pb-9 mb-[100px] md:mb-5" id="contact">
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[43vw]">
+          Ready to take <span className="text-purple">your </span>digital
+          presence to the next level?
+        </h1>
+        <p className="text-cyan-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let's discuss how I can help you in giving
+          perfect insights
+        </p>
+        <a href="mailto:kushagraagrawal128@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Kushagra
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile, link) => (
+            <a
+              key={profile.id}
+              href={profile.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-9 h-9 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 rounded-lg bg-opacity-75 bg-black-200 border border-black-300"
+            >
+              <img
+                src={profile.img}
+                alt={`Social icon ${profile.id}`}
+                width={20}
+                height={20}
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
---
+++ data/index.ts
@@ -0,0 +1,223 @@
+import { link } from "fs";
+
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Feel free for collaboration",
+    description: "",
+    className: "lg:col-span-2 md:col-span-6 md:row-span-2 lg:min-h-[50vh]",
+    imgClassName: "w-full h-full opacity-95",
+    titleClassName: "justify-end",
+    img: "/Collab.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-1 md:col-span-3 md:row-span-2",
+    imgClassName: "w-full h-full opacity-20",
+    titleClassName: "justify-start",
+    img: "/timezone.png",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I try to improve my skills everyday",
+    className: "lg:col-span-2 lg:row-span-1 md:col-span-3 md:row-span-2",
+    imgClassName: "w-full h-full opacity-35",
+    titleClassName: "justify-center",
+    img: "/Linkedin.png",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Budding Data Scientist, looking for opportunity",
+    description: "",
+    className: "lg:col-span-1 md:col-span-3 md:row-span-1",
+    imgClassName: "w-full h-full ",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently learning Data Analysis",
+    description: "The Inside Scoop",
+    className: "lg:col-span-1 lg:row-span-1 md:col-span-3 md:row-span-2",
+    imgClassName: "w-full h-full opacity-30",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/DA.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 lg:row-span-1 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/adrianhajdin?tab=repositories",
+  },
+  {
+    id: 2,
+    title: "Social Media Usage Analysis",
+    des: "Analyzes social media data to track usage and engagement.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/DS-Kushagra?tab=repositories",
+  },
+  {
+    id: 3,
+    title: "Job Scrapping",
+    des: "It provides a basic template for web scraping job listings. Depending on the website's structure, you may need to adjust the HTML element classes or add error handling and more sophisticated scraping logic",
+    img: "/WS.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/DS-Kushagra?tab=repositories",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/adrianhajdin/iphone",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Kushagra has been a fantastic experience. He took the time to understand our needs and delivered a solution that exceeded our expectations. His ability to combine technical expertise with creativity is truly impressive.",
+    name: "Liam D.",
+    title: "CEO of Future Vision",
+  },
+  {
+    quote:
+      "Working with Kushagra has been a fantastic experience. He took the time to understand our needs and delivered a solution that exceeded our expectations. His ability to combine technical expertise with creativity is truly impressive.",
+    name: "Liam D.",
+    title: "CEO of Future Vision",
+  },
+  {
+    quote:
+      "Working with Kushagra has been a fantastic experience. He took the time to understand our needs and delivered a solution that exceeded our expectations. His ability to combine technical expertise with creativity is truly impressive.",
+    name: "Liam D.",
+    title: "CEO of Future Vision",
+  },
+  {
+    quote:
+      "Working with Kushagra has been a fantastic experience. He took the time to understand our needs and delivered a solution that exceeded our expectations. His ability to combine technical expertise with creativity is truly impressive.",
+    name: "Liam D.",
+    title: "CEO of Future Vision",
+  },
+  {
+    quote:
+      "Working with Kushagra has been a fantastic experience. He took the time to understand our needs and delivered a solution that exceeded our expectations. His ability to combine technical expertise with creativity is truly impressive.",
+    name: "Liam D.",
+    title: "CEO of Future Vision",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/DS-Kushagra",
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/_kushagra.ag_",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/kushagra--agrawal",
+  },
+];
+

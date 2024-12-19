99f67b1045b05c8f3dec19579fcb89fbc4691270
Kushagra <kushagraagrawal128@gmail.com>
2024-12-19 21:21:55 +0530

    new updates

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,13 +1,12 @@
-'use client'
+"use client";
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
-import {GlobeDemo} from "./GridGlobe"
+import { GlobeDemo } from "./GridGlobe";
 import { useState } from "react";
-import animationData from '@/data/cofetti.json'
+import animationData from "@/data/cofetti.json";
 import Lottie from "react-lottie";
 import { MagicButton } from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
-
 
 export const BentoGrid = ({
   className,
@@ -19,7 +18,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
         className
       )}
     >
@@ -52,10 +51,10 @@ export const BentoGridItem = ({
   const [copied, setCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText('kushagraagrawal128@gmail.com');
-
+    navigator.clipboard.writeText("kushagraagrawal128@gmail.com");
     setCopied(true);
-  }
+  };
+
   return (
     <div
       className={cn(
@@ -68,7 +67,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -87,7 +86,7 @@ export const BentoGridItem = ({
             <img
               src={spareImg}
               alt={spareImg}
-              className={"object-cover, object-center , w-full h-full"}
+              className={"object-cover object-center w-full h-full"}
             />
           )}
         </div>
@@ -142,23 +141,23 @@ export const BentoGridItem = ({
           {id === 6 && (
             <div className="mt-5 relative">
               <div className={`absolute -bottom-5 right-0`}>
-
-                <Lottie options={{
-                  loop: copied,
-                  autoplay: copied,
-                  animationData,
-                  rendererSettings:{
-                    preserveAspectRatio:'xMidYMid slice'
-                  }
-                }}/>
-
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                />
               </div>
-              <MagicButton 
-              title={copied ? 'Email Copied' : 'Copy my Email'}
-              icon={<IoCopyOutline/>}
-              position="left"
-              otherClasses="!bg-[161a31]"
-              handleClick={handleCopy}
+              <MagicButton
+                title={copied ? "Email Copied" : "Copy my Email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[161a31]"
+                handleClick={handleCopy}
               />
             </div>
           )}
